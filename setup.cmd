8ed874c6b0228d5158f1b1712cc89654f34b7a3f
sunitha muthukrishna <sumuth@microsoft.com>
2017-03-14 18:42:30 -0700

    updated setup.cmd for troubleshooting

--- setup.cmd
+++ setup.cmd
@@ -1,6 +1,5 @@
+env\scripts\pip uninstall -p django
 env\scripts\pip install django
-
 env\scripts\pip install -U pip
-env\scripts\pip install psycopg2
-
-env\scripts\pip install pytz
+env\scripts\pip install -U psycopg2
+env\scripts\pip install -U pytz
